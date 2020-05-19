f18503585f5d7433508f2c0a6798e7d348202f11
smallcoderofme <svensama@outlook.com>
2020-05-19 11:41:17 +0800

    nodejs batch read

---
+++ batch_process/batch_read.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const path = require("path");
+const fs = require('fs');
+
+
+var file_list = [];
+var config = [];
+function get_name(file) {
+	let lastDot = file.lastIndexOf('.');
+	let pre = file.lastIndexOf('\\');
+	return file.slice(pre+1, lastDot);
+}
+function run(file) {
+	let file_name = get_name(file);
+	let data = sharp(file)
+		.jpeg()
+		.limitInputPixels(0)
+		.tile({size: 256})
+		.toFile('big_comp_output/'+file_name, (err, info) => {
+			if (err == null) {
+				if (file_list.length) {
+					console.log(file, 'slice complete.');
+					setTimeout(()=> {
+						run(file_list.shift());
+					}, 1000);
+				}
+			} else {
+				console.log('toFile err:', err);
+			}
+		});
+}
+
+function get_files(file_path){
+	/**
+    function files(path1){
+        fs.readdir(path1,
+        	function(err, files){
+        		if (err) { throw err; }
+		        files.forEach(function (item, index) {
+		        	// console.log(item, index);
+		            let f_path = path.join(path1,item);
+		            fs.stat(f_path, function(err, stat) {
+		            	if (err) { throw err; }
+	            	    if(stat.isDirectory() === true) {
+		                	files(f_path);
+			            }
+			            if (stat.isFile() === true) { 
+			              	file_list.push(f_path);
+	              			console.log('push file', f_path);
+			            }
+		            });
+		        
+		        });
+    	});
+    }
+*/
+    function files(path1){
+     	let files = fs.readdirSync(path1);
+        files.forEach(function (item, index) {
+        	// console.log(item, index);
+            let f_path = path.join(path1,item);
+            let stat = fs.statSync(f_path);
+            if(stat.isDirectory() === true) {
+                files(f_path);
+            }
+            if (stat.isFile() === true) { 
+              file_list.push(f_path);
+            }
+        });
+    }
+
+    files(file_path);
+ 
+ //    file_list.forEach((file) => {
+ //    	config.push(file.replace('inputs\\', '../outputs/').replace('.jpg','.dzi'));
+ //    });
+ //    fs.writeFile('config.json', JSON.stringify(config), (err) => {
+	// 	if (err) throw err;
+	// 	console.log('config was saved');
+	// });
+ 
+    console.log('总共需要裁切的图片数:', file_list.length);
+
+    // if(file_list.length){
+	   //  run(file_list.shift());
+    // }
+}
+
+get_files("big_comp_input");
